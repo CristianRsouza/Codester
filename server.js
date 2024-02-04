fe07e6a213b7a9515da1643ed5cdf546a82c8e26
cristRodsz. <cristianrodriguesouza@hotmail.com>
2024-02-04 07:16:52 -0300

    FEAT: addition of github oauth

--- server.js
+++ server.js
@@ -1,12 +1,60 @@
-import Server  from "express";
-import cors from "cors"
+import express, {json} from "express";
+import cors from "cors";
+import axios from "axios";
+import qs from "query-string";
 
-const App = Server()
+const app = express()
+app.use(cors());
+app.use(json());
 
-const PORT = 3000
+app.post("/login", async (req, res) => {
+  try {
+    const token = await exchangeCodeForAccessToken(req.body.code);
+    console.log("token", token);
 
-App.use(cors())
+    const user = await fetchUser(token);
+    res.send(user);
+  } catch(err) {
+    if (err.response && err.response.data) {
+      console.log("err", err.response.data);
+    } else {
+      console.error("Erro desconhecido:", err);
+    }
+    res.sendStatus(500);
+  }
+});
 
-App.listen(PORT, () => {
-  console.log("SERVER IS RUNNING ON PORT ", PORT);
-})
+
+async function exchangeCodeForAccessToken(code) {
+  const GITHUB_ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
+  const params = {
+    code,
+    grant_type: 'authorization_code',
+    redirect_uri: "http://localhost:5173",
+    client_id: "98347812d33e772baf1e", 
+    client_secret: "2c77a7f6e97f87d0c071c76ac5525f25b5f3c601",
+  };
+
+  const { data } = await axios.post(GITHUB_ACCESS_TOKEN_URL, params, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  });
+
+  const parsedData = qs.parse(data);
+  return parsedData.access_token;
+}
+
+async function fetchUser(token) {
+  const response = await axios.get("https://api.github.com/user", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+app.listen(5000, () => {
+  console.log(`Server is up and running on port 5000`);
+});
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,23 +1,54 @@
-import { useContext } from 'react'
-import './Login.css'
-import { Context } from '../../App'
+import React, { useContext, useEffect } from 'react';
+import './Login.css';
+import { Context } from '../../App';
+import queryString from 'query-string';
+import axios from 'axios';
 
 const Login = () => {
+    const { auth, setAuth } = useContext(Context);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { code } = queryString.parseUrl(window.location.href).query;
+
+            if (code) {
+                try {
+                    const response = await axios.post('http://localhost:5000/login', { code });
+                    const data = await response.data;
+                    console.log(data);
+                    setAuth(true)
+                } catch (error) {
+                    console.error('Erro ao fazer a requisição para o servidor:', error);
+                }
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    
+
+    const redirectToGithub = () => {
+      const GITHUB_URL = "https://github.com/login/oauth/authorize";
+      const params = {
+          response_type: "code",
+          scope: "user",
+          client_id: "98347812d33e772baf1e",
+          redirect_uri: "http://localhost:5173"
+      };
+      const queryStringParams = queryString.stringify(params);
+      const authURL = `${GITHUB_URL}?${queryStringParams}`;
+      window.location.href = authURL;
+  };
   
-  const {auth, setAuth, myUser, setMyUser} = useContext(Context)
 
+    console.log(auth);
 
-
-
-
-  console.log(auth);
-  
-  return(
-    <div className='Login'>
-
-      <button>Login with GitHub</button>
-    </div>
-  )
+    return (
+        <div className='Login'>
+            <button onClick={redirectToGithub}>Login with GitHub</button>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
